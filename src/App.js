ff15676d96c85e79ade2ced8a5cd82aec4a72c7e
goragottsen <goragottsen@gmail.com>
2019-07-20 12:42:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    input: [
+      {length: 0}
+    ]
+  }
+  
+  outputLength = (event) => {
+    const input = [...this.state.input]
+    const inputString = event.target.value;
+    input.length = inputString.length;
+    this.setState({input: input});
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <input 
+          onChange={this.outputLength}
+          type="text"/>
+        <p>{this.state.input.length}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
